980c751c18493b2b5a6288eb6c6ad459aa345b26
smelch-blip <smelch77@gmail.com>
2025-08-29 15:19:49 +0530

    Update Intrinsic-value-app.py

--- Intrinsic-value-app.py
+++ Intrinsic-value-app.py
@@ -1,5 +1,5 @@
-Python 3.13.5 (v3.13.5:6cb20a219a8, Jun 11 2025, 12:23:45) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
-Enter "help" below or click "Help" above for more information.
+
+
 >>> import streamlit as st
 ... import numpy as np
 ... import pandas as pd
